982be33f6b23d9d770b07fab344f23293724220b
DESKTOP-Q2S85GV\Admin <huynhbuingockhoa@gmail.com>
2024-11-14 01:49:03 +0700

    Default Project

---
+++ ossim_source_code/src/queue.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
+}
+
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+        /* TODO: put a new process to queue [q] */
+        if (q == NULL)
+                return;
+        q->proc[q->size] = proc;
+        q->size++;
+}
+
+struct pcb_t *dequeue(struct queue_t *q)
+{
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to remove it from q
+         * */
+        return NULL;
+}
