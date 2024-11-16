02dd234ba5633fa262a827647b16d4209a0302df
DESKTOP-Q2S85GV\Admin <huynhbuingockhoa@gmail.com>
2024-11-16 11:33:44 +0700

    update queue.c & sched.c

--- ossim_source_code/src/queue.c
+++ ossim_source_code/src/queue.c
@@ -12,16 +12,39 @@ int empty(struct queue_t *q)
 void enqueue(struct queue_t *q, struct pcb_t *proc)
 {
         /* TODO: put a new process to queue [q] */
-        if (q == NULL)
+        if (q->size == MAX_QUEUE_SIZE)
                 return;
-        q->proc[q->size] = proc;
-        q->size++;
+        q->proc[q->size++] = proc;
 }
 
 struct pcb_t *dequeue(struct queue_t *q)
 {
-        /* TODO: return a pcb whose prioprity is the highest
+        /* TODO: return a pcb whose priority is the highest
          * in the queue [q] and remember to remove it from q
          * */
+        if (empty(q))
+        {
+                return NULL;
+        }
+        else
+        {
+                int highest_priority = q->proc[0]->priority;
+                int highest_priority_index = 0;
+                for (int i = 1; i < q->size; i++)
+                {
+                        if (q->proc[i]->priority < highest_priority)
+                        {
+                                highest_priority = q->proc[i]->priority;
+                                highest_priority_index = i;
+                        }
+                }
+                struct pcb_t *proc = q->proc[highest_priority_index];
+                for (int i = highest_priority_index; i < q->size - 1; i++)
+                {
+                        q->proc[i] = q->proc[i + 1];
+                }
+                q->size--;
+                return proc;
+        }
         return NULL;
 }
